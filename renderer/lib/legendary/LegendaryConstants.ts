a156c2c6563b35cbb02ea82a8c2ab9aab5dfe871
Gál Péter <pepyta118@gmail.com>
2022-01-02 13:28:40 +0100

    fix: use extraResources to presist binary permissions

--- renderer/lib/legendary/LegendaryConstants.ts
+++ renderer/lib/legendary/LegendaryConstants.ts
@@ -5,22 +5,16 @@ import { isPackaged } from 'electron-is-packaged';
 const isProd = process.env.NODE_ENV === "production";
 const isWindows = platform() === "win32";
 const getLegendaryBin = () => {
-    const fixAsar = (str: string) => {
-        if(!str.includes("app.asar.unpacked")) {
-            return str.replace("app.asar", "app.asar.unpacked");
-        }
 
-        return str;
-    };
 
     const binariesPath =
         isProd && isPackaged // the path to a bundled electron app.
-            ? path.join(process.resourcesPath, '/app.asar.unpacked', '/bin', platform())
+            ? path.join(process.resourcesPath, '/bin', platform())
             : path.join(process.cwd(), '/resources', '/bin', platform());
 
-    return fixAsar(path.resolve(
+    return path.resolve(
         path.join(binariesPath, isWindows ? "/legendary.exe" : "/legendary")
-    ));
+    );
 }
 
 const LegendaryConstants = {
